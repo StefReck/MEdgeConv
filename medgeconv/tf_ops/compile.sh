3a379721e45788378d6323af590e3fa71b2f3455
StefReck <stefanw.reck@web.de>
2021-09-10 14:50:10 +0200

    update req and kernel to tf2.6

--- medgeconv/tf_ops/compile.sh
+++ medgeconv/tf_ops/compile.sh
@@ -1,7 +1,7 @@
 # tf dev docker image; see https://hub.docker.com/r/tensorflow/tensorflow/tags/?page=1&ordering=last_updated&name=custom-op-gpu
-DOCKER_IMAGE="tensorflow/tensorflow:2.5.0-custom-op-gpu-ubuntu16"
+DOCKER_IMAGE="tensorflow/tensorflow:2.6.0-gpu"
 
 # fix the 'cuda_fp16.h: No such file or directory' error:
 FIX_CMND="mkdir -p /usr/local/lib/python3.6/dist-packages/tensorflow/include/third_party/gpus/cuda/include && cp -r /usr/local/cuda/targets/x86_64-linux/include/* /usr/local/lib/python3.6/dist-packages/tensorflow/include/third_party/gpus/cuda/include"
 
-docker run $DOCKER_IMAGE /bin/bash -v "$PWD":/tf_ops -c "${FIX_CMND} && cd tf_ops && make clean && make"
+docker run -v "$PWD":/tf_ops $DOCKER_IMAGE /bin/bash -c "${FIX_CMND} && cd tf_ops && make clean && make"
