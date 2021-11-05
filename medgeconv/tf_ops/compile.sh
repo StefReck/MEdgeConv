d18fee6c60bb994ce1ca4fd9a8ce34b0713c1692
Stefan Reck <33060163+StefReck@users.noreply.github.com>
2021-11-05 13:08:49 +0100

    Tf2.7 (#5)
    * update to tf 2.7
    * remove python 3.6 from tests
    * adjust fix_cmnd in compile.sh for python 3.8 
    * make with c++14

--- medgeconv/tf_ops/compile.sh
+++ medgeconv/tf_ops/compile.sh
@@ -1,7 +1,7 @@
 # tf dev docker image; see https://hub.docker.com/r/tensorflow/tensorflow/tags/
-DOCKER_IMAGE="tensorflow/tensorflow:2.6.0-gpu"
+DOCKER_IMAGE="tensorflow/tensorflow:2.7.0-gpu"
 
 # fix the 'cuda_fp16.h: No such file or directory' error:
-FIX_CMND="mkdir -p /usr/local/lib/python3.6/dist-packages/tensorflow/include/third_party/gpus/cuda/include && cp -r /usr/local/cuda/targets/x86_64-linux/include/* /usr/local/lib/python3.6/dist-packages/tensorflow/include/third_party/gpus/cuda/include"
+FIX_CMND="mkdir -p /usr/local/lib/python3.8/dist-packages/tensorflow/include/third_party/gpus/cuda/include && cp -r /usr/local/cuda/targets/x86_64-linux/include/* /usr/local/lib/python3.8/dist-packages/tensorflow/include/third_party/gpus/cuda/include"
 
 docker run -v "$PWD":/tf_ops $DOCKER_IMAGE /bin/bash -c "${FIX_CMND} && cd tf_ops && make clean && make"
