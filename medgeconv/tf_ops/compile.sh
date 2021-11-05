279eb5921874c689763adad68a644c3433b67e7a
StefReck <stefanw.reck@web.de>
2021-11-05 12:58:17 +0100

    update kernel, adjust fix_cmnd in compile.sh and require c++14

--- medgeconv/tf_ops/compile.sh
+++ medgeconv/tf_ops/compile.sh
@@ -2,6 +2,6 @@
 DOCKER_IMAGE="tensorflow/tensorflow:2.7.0-gpu"
 
 # fix the 'cuda_fp16.h: No such file or directory' error:
-FIX_CMND="mkdir -p /usr/local/lib/python3.6/dist-packages/tensorflow/include/third_party/gpus/cuda/include && cp -r /usr/local/cuda/targets/x86_64-linux/include/* /usr/local/lib/python3.6/dist-packages/tensorflow/include/third_party/gpus/cuda/include"
+FIX_CMND="mkdir -p /usr/local/lib/python3.8/dist-packages/tensorflow/include/third_party/gpus/cuda/include && cp -r /usr/local/cuda/targets/x86_64-linux/include/* /usr/local/lib/python3.8/dist-packages/tensorflow/include/third_party/gpus/cuda/include"
 
 docker run -v "$PWD":/tf_ops $DOCKER_IMAGE /bin/bash -c "${FIX_CMND} && cd tf_ops && make clean && make"
